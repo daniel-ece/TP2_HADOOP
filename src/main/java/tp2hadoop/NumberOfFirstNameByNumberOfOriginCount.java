6b3ad8ad43916d210d69c5caef62a10a098bc6f1
daniel-ece <minhdaniel.95@gmail.com>
2016-10-13 17:57:20 +0200

    add Count number of first name by number of origin NumberOfFirstNameByNumberOfOriginCount.java

---
+++ src/main/java/tp2hadoop/NumberOfFirstNameByNumberOfOriginCount.java
@@ -0,0 +1,79 @@
+package tp2hadoop;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+/**
+ * Created by Daniel on 13/10/2016.
+ */
+
+//Count number of first name by number of origin input: prenoms.csv like(firstname;sexe;nationality1, nationality2, ...;number) ouput: numberOfOrigins, numberOfFristname
+public class NumberOfFirstNameByNumberOfOriginCount {
+
+    //function mapper that return (key, value), value=1
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+        private final static IntWritable one = new IntWritable(1); //value 1
+        private Text word = new Text(); //text
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
+            String itr = value.toString(); //get the value like (firstname;sexe;nationality1, nationality2, ...;number)
+
+            String origins[] = itr.split(";"); // split by ";"
+            String origin[] = origins[2].split(", "); // because he can have mutiple nationalities index:2
+
+            word.set(String.valueOf(origin.length)); //key = number of origins
+            context.write(word, one);
+
+        }
+    }
+
+    //function reducer that involve all the values corresponding to the same key to a unique pair
+    public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+            int sum = 0;
+
+            //simple sum of values
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum); //put the new value
+            context.write(key, result); //output (numberOfOrigins, numberOfFirstname)
+        }
+    }
+
+    //function main
+    public static void main(String[] args) throws Exception {
+
+        Configuration conf = new Configuration(); //provides access to configuration parameters
+
+        //job allows use to configure the job, submit it, control its execution, and query the state
+        Job job = Job.getInstance(conf, "NumberOfFirstnameByNumberOfOriginCount"); //create new job
+        job.setJarByClass(NumberOfFirstNameByNumberOfOriginCount.class); //set the job jar
+
+        job.setMapperClass(NumberOfFirstNameByNumberOfOriginCount.TokenizerMapper.class); //mapper
+        job.setCombinerClass(NumberOfFirstNameByNumberOfOriginCount.IntSumReducer.class); //combiner
+        job.setReducerClass(NumberOfFirstNameByNumberOfOriginCount.IntSumReducer.class); //reducer
+
+        job.setOutputKeyClass(Text.class); //set the key class for job outputs
+        job.setOutputValueClass(IntWritable.class); //set the value class for job outputs
+
+        FileInputFormat.addInputPath(job, new Path(args[0])); //path input
+        FileOutputFormat.setOutputPath(job, new Path(args[1])); //path output
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1); //submit the job, then poll for progress until the job is complete
+    }
+}
